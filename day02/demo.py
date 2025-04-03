7bef7e5690f011ff31029307e6268f7aa841e63b
Shani Rochell <shani.rochell@weizmann.ac.il>
2025-04-03 12:05:01 +0300

    int and str

--- day02/demo.py
+++ day02/demo.py
@@ -7,5 +7,11 @@
 # print("bmi:", bmi)
 
 #name = "John Lee"
-name = input("Enter your name: ")
-print(f"Hello, {name}!")
+# name = input("Enter your name: ")
+# print(f"Hello, {name}!") #f tells to python to put inside the curly {} to input the name
+
+a = 2
+print(a)
+print(type(a))
+print("a+1.5")
+print(type(a+1.5))
