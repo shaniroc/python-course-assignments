285dd5ee9acf87b10db695a16c52132f6cf7e7c8
Shani Rochell <shani.rochell@weizmann.ac.il>
2025-04-03 11:37:24 +0300

    input user name

--- day02/demo.py
+++ day02/demo.py
@@ -1,2 +1,11 @@
-greeting= "Hello, World again!"
-print(greeting)
+# greeting= "Hello, World again!"
+# print(greeting)
+
+# height = 1.75
+# weight = 68.0
+# bmi = weight / (height**2)
+# print("bmi:", bmi)
+
+#name = "John Lee"
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
