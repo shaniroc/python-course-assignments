4d6cce4c16908b150e03e6c4979e1c34a78567bc
Shani Rochell <shani.rochell@weizmann.ac.il>
2025-04-05 16:58:08 +0300

    addition of circumeference and perimeter

--- day02/circle.py
+++ day02/circle.py
@@ -1,7 +1,10 @@
 #Write a program called circle.py that will ask the user for the radius of the circle 
 # and will print the area and the circumeference of the circle.
 
-radius = input("Hello! Please write down the radius of the circle: ")
-area = 3.14 * (float(radius)**2)
-print(f"The area of your circle is: {area}")
-#print(type(radius))
+import math
+print(math.pi)
+
+radius = float(input("Hello! Please write down the radius of the circle: "))
+area = round(math.pi * (radius**2),3)
+circumeference= round(radius*2*math.pi, 3)
+print(f"The area of your circle is: {area} and its circumeference is: {circumeference}")
--- day02/rectangle.py
+++ day02/rectangle.py
@@ -2,10 +2,6 @@
 # and calculate the area and the perimeter of the rectangle.
 
 height_width = list(map(float,input("Enter the height and width of the rectangle separated by space: ").split()))
-area = height_width[0] * height_width[1]
-print(f"The area of your rectangle is: {area}")
-
-
-
-
-
+area = round(height_width[0] * height_width[1],3)
+perimeter = round(2*height_width[0] + 2*height_width[1], 3)
+print(f"The area of your rectangle is: {area} and its perimeter is: {perimeter}")
